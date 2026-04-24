479a0f133ea0d35b487b3cfb7531485c75cd891d
2017000096-MarcoPineda <m.pineda.5bdb@gmail.com>
2026-04-24 12:59:24 -0600

    Add files via upload

---
+++ Computacion-G3-U2/cajasdedialogo/Cajasdedialogo.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.cajasdedialogo;
+
+/**
+ *
+ * @author LABORATORIO
+ */
+public class Cajasdedialogo {
+
+    public static void main(String[] args) {
+       jfrmdialogo1 pine= new jfrmdialogo1();
+       pine.setVisible(true);
+    }
+}
